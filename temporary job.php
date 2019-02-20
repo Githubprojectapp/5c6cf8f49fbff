89422ef0d0ee8dbb2a251d8e86fb0e4a5c9eb62d
testing1 <testing@dispostable.com>
2019-02-20 12:22:26 +0530

    Auto Committed

---
+++ temporary job.php
@@ -0,0 +1,486 @@
+<?php
+
+/*
+
+Template Name: Temporary Job
+
+*/
+
+?>
+
+
+
+<?php get_header(); ?> 
+<div class="row-fluid clearfix">
+
+        <div class="span12 clearfix">
+
+            <div class="ContentPageDivisionIntro">
+
+                <?php
+
+                if (has_post_thumbnail()) {
+
+                    the_post_thumbnail('full');
+
+                } ?>
+
+            </div>
+
+        </div>
+
+    </div>
+	
+	
+	
+	<div class="ContentPageDivisionIntro">
+
+      
+
+            <img src="/wp-content/uploads/2018/04/inner-banner-5.jpg" alt="">
+
+     
+                <?php
+
+                if (has_post_thumbnail()) {
+
+                    the_post_thumbnail('full');
+
+                } ?>
+
+          
+
+       
+    </div>
+
+
+
+<main class="main py-4">
+
+
+
+    <div class="section">
+
+        <div class="container">
+
+            <div class="row">
+
+                <div class="col-lg-12">
+
+                    <h1 class="page-title"><?php the_title(); ?></h1>
+					
+					 <?php the_content(); ?>
+					
+
+                  <div id="wpjb-main" class="wpjb-page-index">
+
+  
+    <table id="wpjb-job-list" class="wpjb-table">
+        <thead>
+            <tr>
+                <th class="wpjb-first hidden-phone"></th>
+                <th class="wpjb-middle">JOB TITLE</th>
+                <th class="wpjb-middle">LOCATION</th>
+                <th class="wpjb-last">POSTED</th>
+            </tr>
+        </thead>
+		
+        <tbody> 
+       <tr class="wpjb-new wpjb-free wpjb-type-full-time wpjb-category-default">
+  
+   <td class="wpjb-column-logo">
+                    <img src="http://joboard.blockchainpeer.com/wp-content/uploads/wpjobboard/job/1241/company-logo/__64x64_Pareto-Fundraising_Master_postitive_RGB.jpg" id="wpjb-logo" alt="">
+            </td>
+
+    <td class="wpjb-column-title">
+
+        <a href="#">	
+Fundraising Strategy and Business Development Director</a>
+
+          <small class="wpjb-sub">Pareto Fundraising</small>
+
+    </td>
+
+    <td class="wpjb-column-location">
+
+
+
+            <div class="wpjb-tooltip">
+
+                <img src="<?php echo get_template_directory_uri() . '/wpjobboard/images/location-red.png' ?>" alt="" class="wpjb-inline-img" />
+
+
+
+               Sydney, Australia
+
+
+            </div>
+
+
+
+        <small class="wpjb-sub job-type">
+
+          Full-time
+
+        </small>
+
+    </td>
+
+    <td class="wpjb-column-date wpjb-last">
+
+       Sep, 25
+
+    </td>
+
+</tr>
+
+<tr class="wpjb-new wpjb-free wpjb-type-full-time wpjb-category-default">
+
+    <td class="wpjb-column-logo">
+
+      
+
+ <div class="wpjb-logo-placeholder" style="background-color: <?php echo $random_logo_colors[array_rand($random_logo_colors)]; ?>"></div>  
+
+          
+
+    </td>
+
+    <td class="wpjb-column-title">
+
+        <a href="#">	
+Fundraising Strategy and Business Development Director</a>
+
+          <small class="wpjb-sub">Pareto Fundraising</small>
+
+    </td>
+
+    <td class="wpjb-column-location">
+
+
+
+            <div class="wpjb-tooltip">
+
+                <img src="<?php echo get_template_directory_uri() . '/wpjobboard/images/location-red.png' ?>" alt="" class="wpjb-inline-img" />
+				
+				
+                    Sydney, Australia
+
+
+            </div>
+
+
+
+        <small class="wpjb-sub job-type">
+
+          Full-time
+
+        </small>
+
+    </td>
+
+    <td class="wpjb-column-date wpjb-last">
+
+       Sep, 25
+
+    </td>
+
+</tr>
+
+<tr class="wpjb-new wpjb-free wpjb-type-full-time wpjb-category-default">
+
+    <td class="wpjb-column-logo">
+
+      
+
+ <div class="wpjb-logo-placeholder" style="background-color: <?php echo $random_logo_colors[array_rand($random_logo_colors)]; ?>"></div>  
+
+          
+
+    </td>
+
+    <td class="wpjb-column-title">
+
+        <a href="#">	
+Fundraising Strategy and Business Development Director</a>
+
+          <small class="wpjb-sub">Pareto Fundraising</small>
+
+    </td>
+
+    <td class="wpjb-column-location">
+
+
+
+            <div class="wpjb-tooltip">
+
+                <img src="<?php echo get_template_directory_uri() . '/wpjobboard/images/location-red.png' ?>" alt="" class="wpjb-inline-img" />
+
+
+
+               Sydney, Australia
+
+
+            </div>
+
+
+
+        <small class="wpjb-sub job-type">
+
+          Full-time
+
+        </small>
+
+    </td>
+
+    <td class="wpjb-column-date wpjb-last">
+
+       Sep, 25
+
+    </td>
+
+</tr>
+
+<tr class="wpjb-new wpjb-free wpjb-type-full-time wpjb-category-default">
+
+    <td class="wpjb-column-logo">
+
+      
+
+ <div class="wpjb-logo-placeholder" style="background-color: <?php echo $random_logo_colors[array_rand($random_logo_colors)]; ?>"></div>  
+
+          
+
+    </td>
+
+    <td class="wpjb-column-title">
+
+        <a href="#">	
+Fundraising Strategy and Business Development Director</a>
+
+          <small class="wpjb-sub">Pareto Fundraising</small>
+
+    </td>
+
+    <td class="wpjb-column-location">
+
+
+
+            <div class="wpjb-tooltip">
+
+                <img src="<?php echo get_template_directory_uri() . '/wpjobboard/images/location-red.png' ?>" alt="" class="wpjb-inline-img" />
+
+
+
+               Sydney, Australia
+
+
+            </div>
+
+
+
+        <small class="wpjb-sub job-type">
+
+          Full-time
+
+        </small>
+
+    </td>
+
+    <td class="wpjb-column-date wpjb-last">
+
+       Sep, 25
+
+    </td>
+
+</tr>
+
+<tr class="wpjb-new wpjb-free wpjb-type-full-time wpjb-category-default">
+
+    <td class="wpjb-column-logo">
+
+      
+
+ <div class="wpjb-logo-placeholder" style="background-color: <?php echo $random_logo_colors[array_rand($random_logo_colors)]; ?>"></div>  
+
+          
+
+    </td>
+
+    <td class="wpjb-column-title">
+
+        <a href="#">	
+Fundraising Strategy and Business Development Director</a>
+
+          <small class="wpjb-sub">Pareto Fundraising</small>
+
+    </td>
+
+    <td class="wpjb-column-location">
+
+
+
+            <div class="wpjb-tooltip">
+
+                <img src="<?php echo get_template_directory_uri() . '/wpjobboard/images/location-red.png' ?>" alt="" class="wpjb-inline-img" />
+
+
+
+               Sydney, Australia
+
+
+            </div>
+
+
+
+        <small class="wpjb-sub job-type">
+
+          Full-time
+
+        </small>
+
+    </td>
+
+    <td class="wpjb-column-date wpjb-last">
+
+       Sep, 25
+
+    </td>
+
+</tr>
+
+<tr class="wpjb-new wpjb-free wpjb-type-full-time wpjb-category-default">
+
+    <td class="wpjb-column-logo">
+
+      
+
+ <div class="wpjb-logo-placeholder" style="background-color: <?php echo $random_logo_colors[array_rand($random_logo_colors)]; ?>"></div>  
+
+          
+
+    </td>
+
+    <td class="wpjb-column-title">
+
+        <a href="#">	
+Fundraising Strategy and Business Development Director</a>
+
+          <small class="wpjb-sub">Pareto Fundraising</small>
+
+    </td>
+
+    <td class="wpjb-column-location">
+
+
+
+            <div class="wpjb-tooltip">
+
+                <img src="<?php echo get_template_directory_uri() . '/wpjobboard/images/location-red.png' ?>" alt="" class="wpjb-inline-img" />
+
+
+
+               Sydney, Australia
+
+
+            </div>
+
+
+
+        <small class="wpjb-sub job-type">
+
+          Full-time
+
+        </small>
+
+    </td>
+
+    <td class="wpjb-column-date wpjb-last">
+
+       Sep, 25
+
+    </td>
+
+</tr>
+
+<tr class="wpjb-new wpjb-free wpjb-type-full-time wpjb-category-default">
+
+    <td class="wpjb-column-logo">
+
+      
+
+ <div class="wpjb-logo-placeholder" style="background-color: <?php echo $random_logo_colors[array_rand($random_logo_colors)]; ?>"></div>  
+
+          
+
+    </td>
+
+    <td class="wpjb-column-title">
+
+        <a href="#">	
+Fundraising Strategy and Business Development Director</a>
+
+          <small class="wpjb-sub">Pareto Fundraising</small>
+
+    </td>
+
+    <td class="wpjb-column-location">
+
+
+
+            <div class="wpjb-tooltip">
+
+                <img src="<?php echo get_template_directory_uri() . '/wpjobboard/images/location-red.png' ?>" alt="" class="wpjb-inline-img" />
+
+
+
+               Sydney, Australia
+
+
+            </div>
+
+
+
+        <small class="wpjb-sub job-type">
+
+          Full-time
+
+        </small>
+
+    </td>
+
+    <td class="wpjb-column-date wpjb-last">
+
+       Sep, 25
+
+    </td>
+
+</tr>
+
+        </tbody>
+    </table>
+
+    <div id="wpjb-paginate-links">
+        <?php wpjb_paginate_links($url, $result->pages, $result->page, $query) ?>
+    </div>
+
+
+</div>
+
+                </div>
+
+             
+
+            </div>
+
+        </div>
+
+    </div>
+
+
+
+</main>
+
+
+
+<?php get_footer(); ?>
